5934db6ef194fd3314face6b7484efb4d42f3a52
nandini-agrawal-2024 <nandini.agrawal.2024@gmial.com>
2024-03-29 09:58:33 +0530

    job app crud without ajax added

--- src/routes/jobappAjax.js
+++ src/routes/jobappAjax.js
@@ -1,4 +1,4 @@
-// JOb App CRUD Ajax
+// JOb App CRUD - No Ajax
 
 var express = require("express")
 const con = require('../config/connect')
@@ -6,11 +6,13 @@ const con = require('../config/connect')
 const login = express.Router();
 const 
 {
-    getJobForm,postInsertJob
+    getJobForm,postJobForm,getEmpData,updateFormPost
 } = require('../controller/JobApp')
 
 
-login.route("/form").get(getJobForm);
-login.route("/submitData").post(postInsertJob);
+login.route("/formValid").get(getJobForm);
+login.route("/submitData").post(postJobForm);
+login.route("/submitData/:emp_id").get(getEmpData);
+login.route("/submitData/:emp_id/update").post(updateFormPost);
 
 module.exports = login;
